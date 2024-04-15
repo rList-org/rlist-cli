71b7106273bcbc044dd8610dcb60cfa2a8f17993
haruki <meteo.haru@gmail.com>
2024-04-15 12:13:30 +0100

    init

--- src/drivers/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod onedrive;
-
-// If you want to implement a new driver, you must implement the `CloudDriver` trait and `GetVfs` trait.
-
-
-pub enum DriverConfig {
-    /// `driver_type` is `onedrive`.
-    Onedrive(onedrive::OnedriveConfig),
-}
--- src/handlers/mod.rs
+++
@@ -1,5 +0,0 @@
-use std::sync::Arc;
-use rlist_vfs::Wheel;
-use actix_web::web;
-
-pub type WheelData = web::Data<Arc<Wheel>>;
